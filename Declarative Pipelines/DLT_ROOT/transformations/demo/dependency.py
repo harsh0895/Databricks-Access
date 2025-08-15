f82df108039180418475171f2a6d1547651bb2df
hb755037@gmail.com <hb755037@gmail.com>
2025-08-15 18:06:46 +0000

    Created Gold Layer

--- Declarative Pipelines/DLT_ROOT/transformations/demo/dependency.py
+++ Declarative Pipelines/DLT_ROOT/transformations/demo/dependency.py
@@ -1,30 +1,30 @@
-# # Creating an End-to-End basic pipeline
-# import dlt
-# from pyspark.sql.functions import *
+# Creating an End-to-End basic pipeline
+import dlt
+from pyspark.sql.functions import *
 
-# # staging area
-# @dlt.table(
-#     name = "staging_area"
-# )
-# def staging_area():
-#     df = spark.readStream.table("dltharsh.source.orders")
-#     return df
+# staging area
+@dlt.table(
+    name = "staging_area"
+)
+def staging_area():
+    df = spark.readStream.table("dltharsh.source.orders")
+    return df
 
-# # creating transformed area
-# @dlt.view(
-#     name = "transformed_area"
-# )
-# def transformed_area():
-#     df = spark.readStream.table("staging_area")
-#     df = df.withColumn("order_status", upper(col("order_status")))
-#     return df
+# creating transformed area
+@dlt.view(
+    name = "transformed_area"
+)
+def transformed_area():
+    df = spark.readStream.table("staging_area")
+    df = df.withColumn("order_status", upper(col("order_status")))
+    return df
 
-# # creating aggregated area
-# @dlt.table(
-#     name = "aggregated_area"
-# )
-# def aggregated_area():
-#     df = spark.readStream.table("transformed_area")
-#     df = df.groupBy("order_status").count()
-#     return df
+# creating aggregated area
+@dlt.table(
+    name = "aggregated_area"
+)
+def aggregated_area():
+    df = spark.readStream.table("transformed_area")
+    df = df.groupBy("order_status").count()
+    return df
 
